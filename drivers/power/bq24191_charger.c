36ceee90ba8d5283d6fc0d48e474494eb3842b28
himihee.seo <himihee.seo@samsung.com>
2012-03-09 22:24:15 +0900

    BQ24191: charger: add bq24191 charger driver
    add bq24191 charger driver
    
    Change-Id: Idd77a901a22a0216aef48e16caacbac524590af2
    Signed-off-by: himihee.seo <himihee.seo@samsung.com>

---
+++ drivers/power/bq24191_charger.c
@@ -0,0 +1,281 @@
+/*
+ *  bq24191_charger.c
+ *  TI BQ24191 Charger Driver
+ *
+ *  Copyright (C) 2012 Samsung Electronics
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/err.h>
+#include <linux/delay.h>
+#include <linux/gpio.h>
+#include <linux/irq.h>
+#include <linux/interrupt.h>
+#include <linux/i2c.h>
+#include <linux/slab.h>
+#include <linux/platform_data/bq24191_charger.h>
+
+struct bq24191_chg_data {
+	struct i2c_client *client;
+	struct bq24191_platform_data *pdata;
+	struct bq24191_chg_callbacks callbacks;
+};
+
+/* BQ24191 Registers. */
+#define BQ24191_INPUT_SOURCE_CTRL		0x00
+#define BQ24191_POWERON_CONFIG			0x01
+#define BQ24191_CHARGE_CURRENT_CTRL		0x02
+#define BQ24191_PRECHARGE_TERMINATION		0x03
+#define BQ24191_CHARGE_VOLTAGE_CTRL		0x04
+#define BQ24191_TERMINATION_TIMER_CTRL		0x05
+#define BQ24191_IR_COMP_THERMAL_REG		0x06
+#define BQ24191_MISC_OPERATION			0x07
+#define BQ24191_SYSTEM_STATUS			0x08
+#define BQ24191_FAULT				0x09
+#define BQ24191_VENDOR_PART_REV			0x0A
+
+#define BQ24191_CHARGING_ENABLE			0x20
+#define BQ24191_CHARGING_DONE			0x30
+
+static int bq24191_i2c_write(struct i2c_client *client, int reg, u8 value)
+{
+	int ret;
+	ret = i2c_smbus_write_byte_data(client, reg, value);
+	if (ret < 0)
+		dev_err(&client->dev, "%s: Error(%d)\n", __func__, ret);
+	return ret;
+}
+
+static int bq24191_i2c_read(struct i2c_client *client, int reg, u8 *buf)
+{
+	int ret;
+	ret = i2c_smbus_read_i2c_block_data(client, reg, 1, buf);
+	if (ret < 0)
+		dev_err(&client->dev, "%s: Error(%d)\n", __func__, ret);
+	return ret;
+}
+
+static void bq24191_dump_regs(struct i2c_client *client)
+{
+	u8 data = 0;
+	u32 addr = 0;
+	int ret;
+	for (addr = 0; addr <= 0x0A; addr++) {
+		ret = bq24191_i2c_read(client, addr, &data);
+
+		if (ret < 0) {
+			dev_err(&client->dev,
+				"%s: addr(0x%02x) read err(%d)\n", __func__,
+				addr, ret);
+		} else {
+			dev_info(&client->dev,
+				 "%s: addr(0x%02x) data(0x%02x)\n", __func__,
+				 addr, data);
+		}
+	}
+}
+
+static int bq24191_set_charging_enable(struct bq24191_chg_callbacks *ptr,
+				       bool en)
+{
+	struct bq24191_chg_data *chg;
+	struct i2c_client *client;
+	u8 data = 0;
+	int ret;
+
+	if (!ptr) {
+		dev_err(&client->dev, "%s: null callbacks\n", __func__);
+		return -EINVAL;
+	}
+
+	chg = container_of(ptr, struct bq24191_chg_data, callbacks);
+	client = chg->client;
+
+	if (en)
+		data = chg->pdata->chg_enable;
+	else
+		data = chg->pdata->chg_disable;
+
+	ret = bq24191_i2c_write(client, BQ24191_POWERON_CONFIG, data);
+	if (ret < 0)
+		return -EIO;
+	gpio_set_value(chg->pdata->gpio_ta_en, !en);
+
+	return 0;
+}
+
+static int bq24191_set_charging_current(struct bq24191_chg_callbacks *ptr,
+					int cable_type)
+{
+	struct bq24191_chg_data *chg;
+	struct i2c_client *client;
+	u8 data = 0;
+	int ret;
+
+	if (!ptr) {
+		dev_err(&client->dev, "%s: null callbacks\n", __func__);
+		return -EINVAL;
+	}
+
+	chg = container_of(ptr, struct bq24191_chg_data, callbacks);
+	client = chg->client;
+
+	if (cable_type == CHARGER_AC)
+		data = chg->pdata->high_current_charging;
+	else
+		data = chg->pdata->low_current_charging;
+
+	ret = bq24191_i2c_write(client, BQ24191_INPUT_SOURCE_CTRL, data);
+	if (ret < 0)
+		return -EIO;
+
+	return 0;
+}
+
+static void __devinit bq24191_init_register(struct i2c_client *client)
+{
+	int ret;
+
+	/* Charge current set 2036mA */
+	ret = bq24191_i2c_write(client, BQ24191_CHARGE_CURRENT_CTRL, 0x60);
+	if (ret < 0)
+		dev_err(&client->dev, "%s: err : addr(%d)\n",
+			__func__, BQ24191_CHARGE_CURRENT_CTRL);
+
+	/* Disable auto termination, Disable timer */
+	ret = bq24191_i2c_write(client, BQ24191_TERMINATION_TIMER_CTRL, 0x00);
+	if (ret < 0)
+		dev_err(&client->dev, "%s: err : addr(%d)\n",
+			__func__, BQ24191_TERMINATION_TIMER_CTRL);
+}
+
+static irqreturn_t bq24191_pg_int_intr_handler(int irq, void *arg)
+{
+	struct bq24191_chg_data *chg = (struct bq24191_chg_data *)arg;
+	struct i2c_client *client = chg->client;
+	int gpio_ta_int = gpio_get_value(chg->pdata->gpio_ta_int);
+
+	dev_dbg(&client->dev, "%s: (%d)\n", __func__, gpio_ta_int);
+
+	if (chg->pdata->change_cable_status)
+		chg->pdata->change_cable_status(gpio_ta_int);
+
+	return IRQ_HANDLED;
+}
+
+static int __devinit bq24191_charger_i2c_probe(struct i2c_client *client,
+					const struct i2c_device_id *id)
+{
+	struct i2c_adapter *adapter = to_i2c_adapter(client->dev.parent);
+	struct bq24191_chg_data *chg;
+	int ret = 0;
+
+	if (!i2c_check_functionality(adapter, I2C_FUNC_SMBUS_BYTE))
+		return -EIO;
+
+	dev_info(&client->dev, "bq24191 Charger Driver Loading\n");
+
+	chg = kzalloc(sizeof(*chg), GFP_KERNEL);
+	if (!chg)
+		return -ENOMEM;
+
+	chg->client = client;
+	chg->pdata = client->dev.platform_data;
+	if (!chg->pdata) {
+		dev_err(&client->dev, "%s: No platform data supplied\n",
+			__func__);
+		ret = -EINVAL;
+		goto err_pdata;
+	}
+
+	i2c_set_clientdata(client, chg);
+	bq24191_init_register(client);
+
+	chg->callbacks.set_charging_current = bq24191_set_charging_current;
+	chg->callbacks.set_charging_enable = bq24191_set_charging_enable;
+	if (chg->pdata && chg->pdata->register_callbacks)
+		chg->pdata->register_callbacks(&chg->callbacks);
+
+	ret = request_threaded_irq(client->irq, NULL,
+		bq24191_pg_int_intr_handler,
+		IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, "bq24191 /pg", chg);
+	if (ret) {
+		dev_err(&client->dev, "%s: /pg irq register failed(%d)!\n",
+			__func__, gpio_to_irq(chg->pdata->gpio_ta_int));
+		goto err_charger_irq;
+	}
+
+	if (chg->pdata->change_cable_status)
+		chg->pdata->change_cable_status
+		    (gpio_get_value(chg->pdata->gpio_ta_int));
+
+	return 0;
+
+err_charger_irq:
+	if (chg->pdata->unregister_callbacks)
+		chg->pdata->unregister_callbacks();
+err_pdata:
+	kfree(chg);
+	i2c_set_clientdata(client, NULL);
+	return ret;
+}
+
+static int __devexit bq24191_charger_remove(struct i2c_client *client)
+{
+	struct bq24191_chg_data *chg = i2c_get_clientdata(client);
+
+	free_irq(gpio_to_irq(chg->pdata->gpio_ta_int), NULL);
+
+	if (chg->pdata && chg->pdata->unregister_callbacks)
+		chg->pdata->unregister_callbacks();
+
+	kfree(chg);
+	return 0;
+}
+
+static const struct i2c_device_id bq24191_charger_id[] = {
+	{"bq24191-charger", 0},
+	{}
+};
+
+static struct i2c_driver bq24191_charger_i2c_driver = {
+	.driver = {
+		   .owner = THIS_MODULE,
+		   .name = "bq24191-charger",
+		   },
+	.id_table = bq24191_charger_id,
+	.probe = bq24191_charger_i2c_probe,
+	.remove = __devexit_p(bq24191_charger_remove),
+	.command = NULL,
+};
+
+MODULE_DEVICE_TABLE(i2c, bq24191_charger_id);
+
+static int __init bq24191_charger_init(void)
+{
+	int ret = i2c_add_driver(&bq24191_charger_i2c_driver);
+
+	if (ret < 0)
+		pr_err("%s: failed to add bq24191 i2c drv\n", __func__);
+
+	return ret;
+}
+
+static void __exit bq24191_charger_exit(void)
+{
+	i2c_del_driver(&bq24191_charger_i2c_driver);
+}
+
+module_init(bq24191_charger_init);
+module_exit(bq24191_charger_exit);
+
+MODULE_AUTHOR("Mihee Seo <himihee.seo@samsung.com>");
+MODULE_DESCRIPTION("bq24191 charger driver");
+MODULE_LICENSE("GPL");
---
+++ include/linux/platform_data/bq24191_charger.h
@@ -0,0 +1,46 @@
+/*
+ * bq24191_charger.h
+ *
+ * Copyright (C) 2012 Samsung Electronics, Inc.
+ *
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __BQ24191_CHARGER_H
+#define __BQ24191_CHARGER_H
+
+enum {
+	CHARGER_BATTERY = 0,
+	CHARGER_AC,
+	CHARGER_USB,
+	CHARGER_DOCK,
+};
+
+struct bq24191_chg_callbacks {
+	int (*set_charging_current) (struct bq24191_chg_callbacks *, int);
+	int (*set_charging_enable) (struct bq24191_chg_callbacks *, bool);
+};
+
+struct bq24191_platform_data {
+	void (*register_callbacks)(struct bq24191_chg_callbacks *);
+	void (*unregister_callbacks)(void);
+	void (*change_cable_status)(int);
+	u8 high_current_charging;
+	u8 low_current_charging;
+	u8 chg_enable;
+	u8 chg_disable;
+	int gpio_ta_int;
+	int gpio_ta_nchg;
+	int gpio_ta_en;
+};
+
+#endif /* __BQ24191_CHARGER_H */
