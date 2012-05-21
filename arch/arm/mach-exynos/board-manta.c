76f9da31fa1132cebaa721ac1fdb95831ae9361c
Dmitry Shmidt <dimitrysh@google.com>
2012-05-21 13:25:29 -0700

    ARM: exynos: manta: Set IO timing value for dwmci0
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- arch/arm/mach-exynos/board-manta.c
+++ arch/arm/mach-exynos/board-manta.c
@@ -254,6 +254,8 @@ static struct dw_mci_board exynos_dwmci0_pdata __initdata = {
 	.hclk_name		= "dwmci",
 	.cclk_name		= "sclk_dwmci",
 	.cfg_gpio		= exynos_dwmci0_cfg_gpio,
+	.sdr_timing		= 0x03020001,
+	.ddr_timing		= 0x03030002,
 };
 
 static struct platform_device *manta_devices[] __initdata = {
