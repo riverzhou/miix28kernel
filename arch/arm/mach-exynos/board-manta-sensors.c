ea1d67271d356e0a617b2332ffaa32152ff2a825
Jinkyu Song <jksong@sta.samsung.com>
2012-06-12 15:03:16 -0700

    ARM: exynos: manta: Enable MPU6050 motion sensor in board file
    Enabling Invensense MPU6050 driver which also handles AK8963
    compass sensor data.
    
    Change-Id: I99e47c2d247ff9edf23a15b83a7d69403b1b61fc
    Signed-off-by: Jinkyu Song <jksong@sta.samsung.com>

--- arch/arm/mach-exynos/board-manta-sensors.c
+++ arch/arm/mach-exynos/board-manta-sensors.c
@@ -15,11 +15,43 @@
 #include <linux/kernel.h>
 #include <linux/i2c.h>
 #include <linux/gpio.h>
+#include <linux/mpu.h>
 #include <plat/gpio-cfg.h>
+
+#define GPIO_ACC_INT	EXYNOS5_GPX1(4)
+#define EINT_ACC	12
+#define GPIO_MSENSE_RST	EXYNOS5_GPG2(0)
+
+static struct mpu_platform_data mpu_data = {
+	.int_config  = 0x00,
+	.level_shifter = 0,
+	.orientation = {
+		0,  1,  0,
+		1,  0,  0,
+		0,  0, -1,
+	},
+	.sec_slave_type = SECONDARY_SLAVE_TYPE_COMPASS,
+	.sec_slave_id   = COMPASS_ID_AK8963,
+	.secondary_i2c_addr = 0x0C,
+	.secondary_orientation = {
+		-1,  0,  0,
+		 0,  1,  0,
+		 0,  0, -1,
+	},
+	.key = {
+		221,  22, 205,   7, 217, 186, 151, 55,
+		206, 254,  35, 144, 225, 102,  47, 50,
+	},
+};
 
 static struct i2c_board_info __initdata manta_sensors_i2c1_boardinfo[] = {
 	{
 		I2C_BOARD_INFO("bmp182", 0x77),
+	},
+	{
+		I2C_BOARD_INFO("mpu6050", 0x68),
+		.irq = IRQ_EINT(EINT_ACC),
+		.platform_data = &mpu_data,
 	},
 };
 
@@ -38,6 +70,11 @@ static struct i2c_board_info __initdata manta_sensors_i2c2_boardinfo[] = {
 
 void __init exynos5_manta_sensors_init(void)
 {
+	s3c_gpio_setpull(GPIO_ACC_INT, S3C_GPIO_PULL_UP);
+
+	gpio_request_one(GPIO_ACC_INT, GPIOF_IN, "ACC_INT");
+	gpio_request_one(GPIO_MSENSE_RST, GPIOF_OUT_INIT_HIGH, "MSENSE_RST");
+
 	i2c_register_board_info(1, manta_sensors_i2c1_boardinfo,
 		ARRAY_SIZE(manta_sensors_i2c1_boardinfo));
 
