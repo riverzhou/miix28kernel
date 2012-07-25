19f53039bb66193462cae0b66f96112a8edb0465
Benoit Goby <benoit@android.com>
2012-07-24 19:07:54 -0700

    ARM: manta: connector: Add support for host mode
    Change-Id: I39781cf83841dade3cfd71753401831c08f20619
    Signed-off-by: Benoit Goby <benoit@android.com>

--- arch/arm/mach-exynos/board-manta-connector.c
+++ arch/arm/mach-exynos/board-manta-connector.c
@@ -19,7 +19,9 @@
 #include <linux/interrupt.h>
 #include <linux/kthread.h>
 #include <linux/module.h>
+#include <linux/usb.h>
 #include <linux/usb/gadget.h>
+#include <linux/usb/hcd.h>
 #include <linux/usb/otg.h>
 
 #include <plat/gpio-cfg.h>
@@ -35,7 +37,9 @@ struct manta_otg {
 	struct usb_otg		otg;
 	struct usb_phy		phy;
 	struct delayed_work	work;
+	struct mutex		lock;
 	bool			usb_connected;
+	struct usb_bus		*ohci;
 
 	/* HACK: s5p phy interface requires passing a pdev pointer */
 	struct platform_device	pdev;
@@ -56,14 +60,114 @@ static int manta_phy_init(struct usb_phy *phy)
 static void manta_phy_shutdown(struct usb_phy *phy)
 {
 	struct manta_otg *motg = container_of(phy, struct manta_otg, phy);
-	s5p_usb_phy_exit(&motg->pdev, S5P_USB_PHY_DEVICE);
+
+	if (motg->phy.state == OTG_STATE_A_HOST)
+		s5p_usb_phy_exit(&motg->pdev, S5P_USB_PHY_HOST);
+	else
+		s5p_usb_phy_exit(&motg->pdev, S5P_USB_PHY_DEVICE);
+}
+
+static int manta_otg_host_enable(struct manta_otg *motg)
+{
+#ifdef CONFIG_USB
+	struct usb_hcd *hcd;
+	struct usb_hcd *ohci_hcd = NULL;
+	int err;
+
+	hcd = bus_to_hcd(motg->otg.host);
+	if (motg->ohci)
+		ohci_hcd = bus_to_hcd(motg->ohci);
+
+	usb_phy_init(&motg->phy);
+
+	err = usb_add_hcd(hcd, hcd->irq, IRQF_SHARED);
+	if (err) {
+		pr_err("failed to add ehci: %d\n", err);
+		goto err_ehci;
+	}
+
+	if (ohci_hcd) {
+		err = usb_add_hcd(ohci_hcd, hcd->irq, IRQF_SHARED);
+		if (err) {
+			pr_err("failed to add ohci: %d\n", err);
+			goto err_ohci;
+		}
+	}
+
+	err = otg_set_vbus(&motg->otg, true);
+	if (err) {
+		pr_err("failed to enable vbus: %d\n", err);
+		goto err_vbus;
+	}
+
+	return 0;
+
+err_vbus:
+	if (ohci_hcd)
+		usb_remove_hcd(ohci_hcd);
+
+err_ohci:
+	usb_remove_hcd(hcd);
+
+err_ehci:
+	usb_phy_shutdown(&motg->phy);
+
+	return err;
+#else
+	return 0;
+#endif
+}
+
+static void manta_otg_host_disable(struct manta_otg *motg)
+{
+#ifdef CONFIG_USB
+	struct usb_hcd *hcd;
+	struct usb_hcd *ohci_hcd = NULL;
+	int err;
+
+	hcd = bus_to_hcd(motg->otg.host);
+	if (motg->ohci)
+		ohci_hcd = bus_to_hcd(motg->ohci);
+
+	err = otg_set_vbus(&motg->otg, false);
+	if (err)
+		pr_err("failed to disable vbus: %d\n", err);
+
+	usb_remove_hcd(hcd);
+	if (ohci_hcd)
+		usb_remove_hcd(ohci_hcd);
+	usb_phy_shutdown(&motg->phy);
+#endif
 }
 
 static int manta_otg_set_host(struct usb_otg *otg, struct usb_bus *host)
 {
+#ifdef CONFIG_USB
+	struct manta_otg *motg = container_of(otg, struct manta_otg, otg);
+	struct usb_hcd *hcd = bus_to_hcd(host);
+
+	mutex_lock(&motg->lock);
+
+	/* HACK to support for ohci */
+	if (host && otg->host) {
+		motg->ohci = host;
+		usb_remove_hcd(hcd);
+		usb_phy_shutdown(&motg->phy);
+		goto out;
+	}
+
 	otg->host = host;
-	if (!host)
-		otg->phy->state = OTG_STATE_UNDEFINED;
+	if (host) {
+		usb_remove_hcd(bus_to_hcd(host));
+		usb_phy_shutdown(&motg->phy);
+	} else {
+		if (otg->phy->state == OTG_STATE_A_HOST)
+			otg->phy->state = OTG_STATE_UNDEFINED;
+	}
+
+out:
+	mutex_unlock(&motg->lock);
+#endif
 	return 0;
 }
 
@@ -72,13 +176,18 @@ static int manta_otg_set_peripheral(struct usb_otg *otg,
 {
 	struct manta_otg *motg = container_of(otg, struct manta_otg, otg);
 
+	mutex_lock(&motg->lock);
+
 	otg->gadget = gadget;
+	if (gadget) {
+		if (otg->phy->state == OTG_STATE_B_PERIPHERAL)
+			usb_gadget_vbus_connect(motg->otg.gadget);
+	} else {
+		if (otg->phy->state == OTG_STATE_B_PERIPHERAL)
+			otg->phy->state = OTG_STATE_UNDEFINED;
+	}
 
-	if (otg->phy->state == OTG_STATE_UNDEFINED)
-		queue_delayed_work(system_nrt_wq, &motg->work, 0);
-
-	if (!gadget)
-		otg->phy->state = OTG_STATE_UNDEFINED;
+	mutex_unlock(&motg->lock);
 
 	return 0;
 }
@@ -92,9 +201,12 @@ static int manta_otg_set_vbus(struct usb_otg *otg, bool enabled)
 static void manta_otg_work(struct work_struct *work)
 {
 	struct manta_otg *motg = container_of(work, struct manta_otg, work.work);
-	enum usb_otg_state prev_state = motg->phy.state;
-	int id, vbus;
+	enum usb_otg_state prev_state;
+	int id, vbus, err;
 
+	mutex_lock(&motg->lock);
+
+	prev_state = motg->phy.state;
 	vbus = motg->usb_connected;
 	id = gpio_get_value(GPIO_USB_ID);
 
@@ -102,55 +214,64 @@ static void manta_otg_work(struct work_struct *work)
 
 	if (!id) {
 		if (prev_state == OTG_STATE_A_HOST)
-			return;
+			goto out;
 
 		if (!motg->otg.host)
-			return;
+			goto out;
 
 		motg->phy.state = OTG_STATE_A_HOST;
 		motg->phy.last_event = USB_EVENT_ID;
 		atomic_notifier_call_chain(&motg->phy.notifier,
-					USB_EVENT_ID,
-					motg->otg.gadget);
-		otg_set_vbus(&motg->otg, 1);
-	} else if (vbus) {
-		if (prev_state == OTG_STATE_B_PERIPHERAL)
-			return;
+					USB_EVENT_ID, NULL);
 
+		err = manta_otg_host_enable(motg);
+		if (err) {
+			motg->phy.last_event = USB_EVENT_NONE;
+			motg->phy.state = OTG_STATE_B_IDLE;
+			atomic_notifier_call_chain(&motg->phy.notifier,
+						USB_EVENT_NONE, NULL);
+			goto out;
+		}
+
+	} else if (vbus) {
 		if (!motg->otg.gadget)
-			return;
+			goto out;
+
+		if (prev_state == OTG_STATE_B_PERIPHERAL)
+			goto out;
 
 		motg->phy.state = OTG_STATE_B_PERIPHERAL;
 		motg->phy.last_event = USB_EVENT_VBUS;
 		atomic_notifier_call_chain(&motg->phy.notifier,
-					USB_EVENT_VBUS,
-					motg->otg.gadget);
+					USB_EVENT_VBUS, NULL);
 		usb_gadget_vbus_connect(motg->otg.gadget);
 	} else {
 		if (prev_state == OTG_STATE_B_IDLE)
-			return;
+			goto out;
 
 		if (prev_state == OTG_STATE_B_PERIPHERAL && motg->otg.gadget)
 			usb_gadget_vbus_disconnect(motg->otg.gadget);
 
 		if (prev_state == OTG_STATE_A_HOST && motg->otg.host)
-			otg_set_vbus(&motg->otg, 0);
+			manta_otg_host_disable(motg);
 
 		motg->phy.state = OTG_STATE_B_IDLE;
 		motg->phy.last_event = USB_EVENT_NONE;
 		atomic_notifier_call_chain(&motg->phy.notifier,
-					USB_EVENT_NONE,
-					motg->otg.gadget);
+					USB_EVENT_NONE, NULL);
 	}
 
 	pr_info("%s -> %s\n", otg_state_string(prev_state),
 			      otg_state_string(motg->phy.state));
+
+out:
+	mutex_unlock(&motg->lock);
 }
 
 static irqreturn_t manta_otg_irq(int irq, void *data)
 {
 	struct manta_otg *motg = data;
-	queue_delayed_work(system_nrt_wq, &motg->work, 20);
+	queue_delayed_work(system_nrt_wq, &motg->work, msecs_to_jiffies(20));
 	return IRQ_HANDLED;
 }
 
@@ -158,7 +279,7 @@ void manta_otg_set_usb_state(bool connected)
 {
 	struct manta_otg *motg = &manta_otg;
 	motg->usb_connected = connected;
-	queue_delayed_work(system_nrt_wq, &motg->work, 20);
+	queue_delayed_work(system_nrt_wq, &motg->work, msecs_to_jiffies(20));
 }
 
 void exynos5_manta_connector_init(void)
@@ -168,6 +289,7 @@ void exynos5_manta_connector_init(void)
 
 	INIT_DELAYED_WORK(&motg->work, manta_otg_work);
 	ATOMIC_INIT_NOTIFIER_HEAD(&motg->phy.notifier);
+	mutex_init(&motg->lock);
 
 	device_initialize(dev);
 	dev_set_name(dev, "%s", "manta_otg");
@@ -204,6 +326,13 @@ static int __init manta_connector_init(void)
 			IRQF_ONESHOT, "usb_id", motg);
 	if (ret)
 		pr_err("request_irq ID failed: %d\n", ret);
+
+	/*
+	 * HACK: delay the initial otg detection by 4 secs so that it happens
+	 * after the battery driver is ready (this fixes booting with otg cable
+	 * attached)
+	 */
+	queue_delayed_work(system_nrt_wq, &motg->work, msecs_to_jiffies(4000));
 
 	return ret;
 }
