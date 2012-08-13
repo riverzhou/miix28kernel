2e637b57a428f324ea50751d5469999d6ba736bb
Colin Cross <ccross@android.com>
2012-08-13 15:42:30 -0700

    HACK: mali-t6xx: set frequency to 450 MHz during probe
    Call the dvfs function during probe to set the frequency to 450 MHz
    and the corresponding vdd_g3d voltage.  Temporary until GPU DVFS is
    fully enabled.
    
    Change-Id: I2300617f78c594717ea49aaddefe450f25729a4f
    Signed-off-by: Colin Cross <ccross@android.com>

--- drivers/gpu/arm/t6xx/kbase/src/platform/mali_kbase_platform.c
+++ drivers/gpu/arm/t6xx/kbase/src/platform/mali_kbase_platform.c
@@ -1130,6 +1130,8 @@ mali_error kbase_platform_init(struct kbase_device *kbdev)
 	}
 #endif /* CONFIG_REGULATOR */
 
+	/* HACK: set gpu frequency to 450 MHz */
+	kbase_platform_dvfs_set_level(kbdev, 5);
 
 #ifdef CONFIG_MALI_T6XX_DVFS
 	kbase_platform_dvfs_init(kbdev);
