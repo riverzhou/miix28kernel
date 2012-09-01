99d129c39a0d9e77b89dd1ed978659789614299d
Todd Poynor <toddpoynor@google.com>
2012-08-31 18:59:21 -0700

    ARM: exynos: manta: Add temperature thresholds for memory throttling
    To match SMDK5250 values.
    
    Change-Id: Iff2b12c3c90d10f0acec8fe3a8675888eea06121
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- arch/arm/mach-exynos/board-manta.c
+++ arch/arm/mach-exynos/board-manta.c
@@ -319,6 +319,8 @@ static struct tmu_data manta_tmu_pdata __initdata = {
 		.start_throttle		= 80,
 		.start_tripping		= 110,
 		.start_emergency	= 120,
+		.stop_mem_throttle	= 80,
+		.start_mem_throttle	= 85,
 	},
 
 	.efuse_value	= 80,
