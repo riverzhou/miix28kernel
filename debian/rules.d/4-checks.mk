81eb078124696d465622613283b2287bddff07a9
Tim Gardner <tim.gardner@canonical.com>
2013-05-10 09:43:04 -0600

    UBUNTU: Sync debian rules with master-next
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian/rules.d/4-checks.mk
+++ debian/rules.d/4-checks.mk
@@ -1,34 +1,27 @@
 # Check ABI for package against last release (if not same abinum)
-abi-%: $(abidir)/%
-	@# Empty for make to be happy
-$(abidir)/%: $(stampdir)/stamp-build-%
+abi-check-%: $(stampdir)/stamp-build-%
+	@echo Debug: $@
 	install -d $(abidir)
 	sed -e 's/^\(.\+\)[[:space:]]\+\(.\+\)[[:space:]]\(.\+\)$$/\3 \2 \1/'	\
-		$(builddir)/build-$*/Module.symvers | sort > $@
-
-abi-check-%: $(abidir)/%
+		$(builddir)/build-$*/Module.symvers | sort > $(abidir)/$*
 	@perl -f $(DROOT)/scripts/abi-check "$*" "$(prev_abinum)" "$(abinum)" \
 		"$(prev_abidir)" "$(abidir)" "$(skipabi)"
 
 # Check the module list against the last release (always)
-module-%: $(abidir)/%.modules
-	@# Empty for make to be happy
-$(abidir)/%.modules: $(stampdir)/stamp-build-%
+module-check-%: $(stampdir)/stamp-build-%
+	@echo Debug: $@
 	install -d $(abidir)
 	find $(builddir)/build-$*/ -name \*.ko | \
-		sed -e 's/.*\/\([^\/]*\)\.ko/\1/' | sort > $@
-
-module-check-%: $(abidir)/%.modules
+		sed -e 's/.*\/\([^\/]*\)\.ko/\1/' | sort > $(abidir)/$*.modules
 	@perl -f $(DROOT)/scripts/module-check "$*" \
 		"$(prev_abidir)" "$(abidir)" $(skipmodule)
 
-checks-%: abi-check-% module-check-%
-	@# Will be calling more stuff later
+checks-%: module-check-% abi-check-%
+	@echo Debug: $@
 
 # Check the config against the known options list.
 config-prepare-check-%: $(stampdir)/stamp-prepare-tree-%
+	@echo Debug: $@
 	@perl -f $(DROOT)/scripts/config-check \
 		$(builddir)/build-$*/.config "$(arch)" "$*" "$(sharedconfdir)" "$(skipconfig)"
 
-prepare-checks-%: config-prepare-check-%
-	@# Will be calling more stuff later
