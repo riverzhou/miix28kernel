f9d9255b497d6169467c7e866599c182c68f2f47
Tim Gardner <tim.gardner@canonical.com>
2013-06-13 07:47:02 -0600

    UBUNTU: do_tools_perf=true
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian.manta/rules.d/armhf.mk
+++ debian.manta/rules.d/armhf.mk
@@ -13,5 +13,6 @@ do_doc_package	= false
 do_source_package	= false
 do_common_headers_indep = false
 do_libc_dev_package	= false
-do_tools		= false
+do_tools		= true
+do_tools_perf		= true
 disable_d_i		= true
