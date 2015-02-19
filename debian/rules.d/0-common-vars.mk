93124e33e8c3147757652c66860bafe47f5f079d
Paolo Pisati <paolo.pisati@canonical.com>
2015-02-19 09:23:52 +0000

    UBUNTU: raspi2 packaging
    Ignore: yes
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian/rules.d/0-common-vars.mk
+++ debian/rules.d/0-common-vars.mk
@@ -11,7 +11,9 @@ release := $(shell sed -n '1s/^$(src_pkg_name).*(\(.*\)-.*).*$$/\1/p' $(DEBIAN)/
 revisions := $(shell sed -n 's/^$(src_pkg_name)\ .*($(release)-\(.*\)).*$$/\1/p' $(DEBIAN)/changelog | tac)
 revision ?= $(word $(words $(revisions)),$(revisions))
 prev_revisions := $(filter-out $(revision),0.0 $(revisions))
+ifneq ($(prev_revisions),)
 prev_revision := $(word $(words $(prev_revisions)),$(prev_revisions))
+endif
 
 prev_fullver ?= $(shell dpkg-parsechangelog -l$(DEBIAN)/changelog -o1 -c1 | sed -ne 's/^Version: *//p')
 
@@ -126,7 +128,7 @@ stampdir	:= $(CURDIR)/debian/stamps
 bin_pkg_name=linux-image-$(abi_release)
 extra_pkg_name=linux-image-extra-$(abi_release)
 hdrs_pkg_name=linux-headers-$(abi_release)
-indep_hdrs_pkg_name=linux-headers-$(abi_release)
+indep_hdrs_pkg_name=$(src_pkg_name)-headers-$(abi_release)
 
 #
 # The generation of content in the doc package depends on both 'AUTOBUILD=' and
